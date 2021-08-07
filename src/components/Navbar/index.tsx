491f9a8db2719e3965369b3ae86594e33a2e7c29
ga-sr <gabriel.10rocha@hotmail.com>
2021-08-06 21:32:10 -0300

    home

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+const Navbar: React.FC = () => {
+  return (
+    <div style={{ height: "200px", width: "100vw", backgroundColor: "yellow" }}>
+      <p>oi</p>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./styles.css";
 import axios from "axios";
-
+import cartSVG from "../../assets/img/cart.svg";
 interface elemInterface {
   photo: string;
   title: string;
@@ -9,19 +9,60 @@ interface elemInterface {
   price: number;
 }
 
+interface filterInterface {
+  op: string;
+  value: string;
+  order: string;
+  cat: string | undefined;
+}
+
 const Home = () => {
+  const endpoint = "http://c5c11e4aa7aa.ngrok.io";
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([{}]);
+  const [productFilter, setProductFilter] = useState<filterInterface>({
+    op: "",
+    value: "",
+    order: "",
+    cat: "",
+  });
+
   const getProducts = async () => {
-    const res = await axios.get("http://f7263ae9dd8f.ngrok.io/products");
+    let url = `${endpoint}/products/?`;
+    let first = false;
+    if (productFilter.op !== "") {
+      if (first === false) first = true;
+      else url = `${url}&`;
+      url = `${url}op=${productFilter.op}`;
+    }
+    if (productFilter.value !== "") {
+      if (first === false) first = true;
+      else url = `${url}&`;
+      url = `${url}value=${productFilter.value}`;
+    }
+    if (productFilter.order !== "") {
+      if (first === false) first = true;
+      else url = `${url}&`;
+      url = `${url}order=${productFilter.order}`;
+    }
+    if (productFilter.cat !== "") {
+      if (first === false) first = true;
+      else url = `${url}&`;
+      url = `${url}cat=${productFilter.cat}`;
+    }
+    const res = await axios.get(url);
     return res.data;
   };
 
-  const [products, setProducts] = useState([]);
+  const addToCArt = (e: elemInterface) => {
+    setCart((cart) => [...cart, e]);
+  };
 
   useEffect(() => {
     getProducts().then((result) => {
       setProducts(result);
     });
-  }, []);
+  }, [productFilter]);
 
   return (
     <div className="hm-container">
@@ -30,12 +71,96 @@ const Home = () => {
       </div>
       <div className="filter">
         <p>Deseja filtrar sua busca?</p>
-        <button onClick={() => {}}>Todos</button>
-        <button onClick={() => {}}>Higiene</button>
-        <button onClick={() => {}}>Hortifruti</button>
-        <button onClick={() => {}}>Pets</button>
-        <button onClick={() => {}}>Bebidas</button>
-        <button onClick={() => {}}>Congelados</button>
+        <button
+          onClick={() => {
+            setProductFilter((old) => {
+              return { ...old, cat: "" };
+            });
+          }}
+        >
+          Todos
+        </button>
+        <button
+          onClick={() => {
+            setProductFilter((old) => {
+              return { ...old, cat: "1" };
+            });
+          }}
+        >
+          Higiene
+        </button>
+        <button
+          onClick={() => {
+            setProductFilter((old) => {
+              return { ...old, cat: "2" };
+            });
+          }}
+        >
+          Hortifruti
+        </button>
+        <button
+          onClick={() => {
+            setProductFilter((old) => {
+              return { ...old, cat: "3" };
+            });
+          }}
+        >
+          Pets
+        </button>
+        <button
+          onClick={() => {
+            setProductFilter((old) => {
+              return { ...old, cat: "4" };
+            });
+          }}
+        >
+          Bebidas
+        </button>
+        <button
+          onClick={() => {
+            setProductFilter((old) => {
+              return { ...old, cat: "5" };
+            });
+          }}
+        >
+          Congelados
+        </button>
+        <span>Preço:</span>
+        <input
+          type="text"
+          onChange={(evt) => {
+            setProductFilter((old) => {
+              return { ...old, value: evt.target.value };
+            });
+          }}
+        />
+        <select
+          onChange={(evt) => {
+            setProductFilter((old) => {
+              return { ...old, order: evt.target.value };
+            });
+          }}
+        >
+          <option value="ASC">Crescente</option>
+          <option value="DESC">Decrescente</option>
+        </select>
+        <select
+          onChange={(evt) => {
+            setProductFilter((old) => {
+              return { ...old, op: evt.target.value };
+            });
+          }}
+        >
+          <option value="GT">Maior</option>
+          <option value="LT">Menor</option>
+        </select>
+        <img
+          src={cartSVG}
+          alt="cart"
+          onClick={() => {
+            console.log(cart);
+          }}
+        />
       </div>
       <span>Produtos por categoria:</span>
       <div className="productsContainer">
@@ -47,7 +172,7 @@ const Home = () => {
                 <p>{elem.title}</p>
                 <p>{elem.category_name}</p>
                 <span>{elem.price}</span>
-                <button onClick={() => {}}>+</button>
+                <button onClick={() => addToCArt(elem)}>+</button>
               </div>
             );
           })}
